52c2192814c077b44d8e6c72254e021355eb37f8
Kamasu Paul <kamasupaul@gmail.com>
2023-05-09 12:45:15 +0300

    -- change fav ico

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Kamasu Paul';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Kamasu Paul website';
 
 export default function Layout({ children, home }) {
   return (
