33450a706f37c2cf029ed13b92c211d1b02e150a
Kamasu Paul <kamasupaul@gmail.com>
2023-09-11 18:46:11 +0300

    -- add social links

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,18 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          Hi, i am Kamasu Paul. Creator of QuickCodes(10k+ downloads) and many
-          others the world might never know about
+      <p>
+          Hi, i am Kamasu Paul. Creator of{" "}
+          <a href="https://play.google.com/store/apps/details?id=com.quickCodes.quickCodes&hl=en&gl=US">
+            QuickCodes
+          </a>
+          (10k+ downloads) and many others the world might never know about
+        </p>
+        <p>   
+        I tend to think about myself as a creative problem solver, an out-of-the-box thinker, and a lifelong learner.
         </p>
         <p>
-        
-          (This is my new website built with{" "}
-          <a href="https://nextjs.org">Next Js</a>. I moved away from my <a href="https://old.kamasupaul.com">wordpress site to be free.</a>  - I am hoping to showcase some
-          awesome works here)
+        If you're curious to learn more about my projects and read my articles, you can find them easily on <a href="https://github.com/kamasuPaul">GitHub</a> , <a href="https://medium.com/@kamasupaul">Medium</a>, and <a href="https://dev.to/kamasupaul">Dev.to</a>
         </p>
       </section>
       {/* hide blog for now */}
